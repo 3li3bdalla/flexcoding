ebb4473f2f5dcdc3272035b606a546a79aa7d752
3li3bdalla <ali.dev.sd@gmail.com>
2019-08-14 12:49:46 +0300

    update course relahispship

--- app/Relashionship/CourseRelashionships.php
+++ app/Relashionship/CourseRelashionships.php
@@ -5,4 +5,16 @@ namespace App\Relashionships;
 trait CourseRelashionships {
 	
 
+
+	public function videos()
+	{
+		return $this->hasMany(App\Video::class, 'course_id')
+	}
+
+
+	// public function hastags()
+	// {
+	// 	return $this->belongsToMany(App\Video::class, 'course_id')
+	// }
+
 }
--- app/Relashionship/VideoRelashionships.php
+++ app/Relashionship/VideoRelashionships.php
@@ -4,5 +4,19 @@ namespace App\Relashionships;
 
 trait VideoRelashionships {
 	
+	public function course()
+	{
+		return $this->belongsTo(App\Course::class, 'course_id')
+	}
+
+
+
+	public function comments()
+	{
+		return $this->hasMany(App\Comment::class, 'video_id')
+	}
 
 }
+
+
+
